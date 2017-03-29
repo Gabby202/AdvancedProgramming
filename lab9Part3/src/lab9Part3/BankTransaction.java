25b04b32497047bd213968921e632cc253458ffc
Gabby202 <gabbu202@gmail.com>
2017-03-29 13:28:55 +0100

    k

--- lab9Part3/src/lab9Part3/BankTransaction.java
+++ lab9Part3/src/lab9Part3/BankTransaction.java
@@ -5,9 +5,10 @@ public class BankTransaction extends Thread {
 	private static BankAccount account = new BankAccount(10000);
 	
 	public void run() {
-		
+	
 		account.makeWithdrawl(10);
-		
+			
 		System.out.println(Thread.currentThread().getName() + " balance after withdrawal: " + account.getBalance());
+		
 	}
 }
